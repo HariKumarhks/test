ca8e968a66a8ffceac161c9298aa801b46e9ce7e
HariKumarhks <60743341+HariKumarhks@users.noreply.github.com>
2023-01-09 16:47:45 +0530

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// test
+/*
+Created on Fri Dec 23 16:30:04 2022
+@author: Hari
+*/
+
+
+fn main() {
+  // let var: 'static String = "sppld".to_string();
+  println!("Is this Docker  RUST🦀🦀🦀🦀");
+}
+
+
+
+// fn type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
+
+
+ /* 
+ docker build . -t test:01
+ docker run -t test:01
+  */
